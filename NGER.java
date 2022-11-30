e99748dc7235cdfab00e00da2b579a724136b9ab
utkarsh1793 <119281050+utkarsh1793@users.noreply.github.com>
2022-11-30 18:48:14 +0530

    Update and rename README.md to NGER.java

---
+++ NGER.java
@@ -0,0 +1,30 @@
+# utkarsh
+
+import java.util.*;
+
+class NGER{
+      public static int[] nextGreater(int arr[]){
+      Stack<Integer> stack=new Stack<>();
+      int ans[]=new int[arr.length];
+      
+      for(int i=arr.length-1;i>=0;i--){
+          while(!Stack.isEmpty() && stack.peek()<=arr[i]){
+          stack.pop();
+          }
+          if(stack.isEmpty()){
+          ans[i]=-1;
+          }
+          else{
+          ans[i]=stack.peek();
+          }
+          stack.push(arr[i]);
+      }
+      return ans;
+    }
+    public static void main(String[] args){
+    int arr[]={2,3,4,6,1,3,4,9}
+    int ans[]=nextGreater(arr);
+    for(int a : ans){
+    system.out.print(a+" ");
+    }
+   }
